bbce2ab9442771e22f05333f5ba23f20efa4e477
Rasmus <rasmushandersen@users.noreply.github.com>
2023-05-23 21:47:18 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,36 @@
-let Volume = 0
 input.onButtonPressed(Button.A, function () {
-    music.playMelody("- - - - - - - - ", 120)
+    Stepsize += 1
+    if (Stepsize >= 31) {
+        Stepsize = 1
+    }
+    Stepchanged = 1
 })
 input.onButtonPressed(Button.B, function () {
-    Volume += 11
+    Volume += Stepsize
+    if (Volume >= 255) {
+        Volume = 0
+    }
     music.setVolume(Volume)
+    Volchanged = 1
 })
+let Volchanged = 0
+let Stepchanged = 0
+let Volume = 0
+let Stepsize = 0
+Stepsize = 5
+Volume = 100
+music.setVolume(Volume)
+Stepchanged = 0
+Volchanged = 0
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    basic.showNumber(Volume)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.ForeverInBackground)
+    if (Stepchanged >= 1) {
+        basic.showString("S" + Stepsize)
+        Stepchanged = 0
+    }
+    if (Volchanged >= 1) {
+        basic.showString("V" + Stepsize)
+        Volchanged = 0
+    }
 })
