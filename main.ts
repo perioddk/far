153d40a22b766c7f5b2d63c8c2d6f8ace2f0cd88
Rasmus <rasmushandersen@users.noreply.github.com>
2023-05-23 21:04:34 +0200

    Opdater pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let Volume = 0
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("- - - - - - - - ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    Volume += 11
+    music.setVolume(Volume)
+})
 basic.forever(function () {
-	
+    basic.showNumber(Volume)
 })
